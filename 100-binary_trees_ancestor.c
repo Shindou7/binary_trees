0da4812ebab7b2b8521175f2a55180ae0923f283
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-11-02 17:00:13 +0100

    Create 100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - Finds the lowest common
+ * @first: first node.
+ * @second: the second node.
+ * Return: If no common ancestors return NULL.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                                    const binary_tree_t *second)
+{
+    binary_tree_t *ancestor_first, *ancestor_second;
+
+    if (!first || !second)
+        return (NULL);
+    if (first == second)
+        return ((binary_tree_t *)first);
+
+    ancestor_first = first->parent;
+    ancestor_second = second->parent;
+
+    if (first == ancestor_second || !ancestor_first || (!ancestor_first->parent && ancestor_second))
+        return (binary_trees_ancestor(first, ancestor_second));
+    else if (ancestor_first == second || !ancestor_second || (!ancestor_second->parent && ancestor_first))
+        return (binary_trees_ancestor(ancestor_first, second));
+    return (binary_trees_ancestor(ancestor_first, ancestor_second));
+}
