45f6dbbd2747b289e9f38c840b43fd7bf0f4e73e
Shindou7 <sh.shindou@gmail.com>
2023-11-02 08:46:18 -0700

    fh

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,18 +7,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  int left_height;
-  int right_height;
+	int left_height;
+	int right_height;
 
-    if (tree)
-    {
-        left_height = measure_height(tree->left);
-        right_height = measure_height(tree->right);
-
-        return (left_height - right_height);
-    }
-
-    return (0);
+	if (tree)
+	{
+		left_height = measure_height(tree->left);
+		right_height = measure_height(tree->right);
+		return (left_height - right_height);
+	}
+	return (0);
 }
 
 /**
@@ -30,14 +28,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 int measure_height(const binary_tree_t *tree)
 {
-  int left_height,right_height;
+	int left_ht, right_ht;
 
-    if (tree)
-    {
-        left_height = (tree->left) ? 1 + measure_height(tree->left) : 1;
-        right_height = (tree->right) ? 1 + measure_height(tree->right) : 1;
-
-        return ((left_height > right_height) ? left_height : right_height;)
-    }
-    return (0);
+	if (tree)
+	{
+		left_ht = (tree->left) ? 1 + measure_height(tree->left) : 1;
+		right_ht = (tree->right) ? 1 + measure_height(tree->right) : 1;
+		return ((left_ht > right_ht) ? left_ht : right_ht;)
+	}
+	return (0);
 }
