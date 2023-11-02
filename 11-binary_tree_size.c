f9913da1adf6954d96acdd56e228c4151967cc0b
Shindou7 <sh.shindou@gmail.com>
2023-11-02 08:54:22 -0700

    gg

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t tree_size = 0;
+	size_t tree_size = 0;
 
-    if (tree)
-    {
-        tree_size += 1;
-        tree_size += binary_tree_size(tree->left);
-        tree_size += binary_tree_size(tree->right);
-    }
-    return (tree_size);
+	if (tree)
+	{
+		tree_size += 1;
+		tree_size += binary_tree_size(tree->left);
+		tree_size += binary_tree_size(tree->right);
+	}
+	return (tree_size);
 }
